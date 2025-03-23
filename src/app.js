122e004b5352525aa6b3374c1c0e7bd740e30318
Jay Ahirrao 11 <jayahirrao11@gmail.com>
2025-03-23 12:12:07 +0530

    added express configurations

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
     origin :process.env.CORS_ORIGIN ,
     credentials:true
 }))
+
+app.use(express.json({limit:"10mb"}));
+app.use(urlencoded({extended:true,limit:"10mb"}));
+app.use(express.static('public'));
+app.use(cookieParser());
 
 export {app}
