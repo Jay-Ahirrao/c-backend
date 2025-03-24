cdf0a447ff93991608880628d1b344276ce4cf38
Jay Ahirrao 11 <jayahirrao11@gmail.com>
2025-03-24 20:18:26 +0530

    Added asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => (req,res,next ) => {
+    Promise.resolve(requestHandler(req,res,next)).reject((error) => {next(error)})
+}
+
+export {asyncHandler}
+
+
+//notes
+// const asyncHandler =  () => {}
+// const asyncHandler =  (fn) => { () => {} }
+// const asyncHandler =  (fn) => () => { }           // just removed braces from previous code !!!
+/*
+const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        req.status(error.code || 500).json({success:false,message:error.message})
+    }
+}   
+*/        
