cc8f6dbf3a97faa61bbe907c10ff591e0f52d58c
Jay Ahirrao 11 <jayahirrao11@gmail.com>
2025-03-29 18:07:52 +0530

    utils folder code discussion

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 import {DB_NAME} from './constants.js';
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv';
+import app from './app.js';
 dotenv.config({
     path:'../.env'
 });
 
 connectDB()
 .then(() => {
-
     app.on((err)=>{
         console.log("Error in starting server !!!",err);
         throw err;
