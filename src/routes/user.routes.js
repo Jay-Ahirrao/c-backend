51bc3fe84d9fdb465411e810e1e2763a49865406
Jay Ahirrao 11 <jayahirrao11@gmail.com>
2025-10-02 20:28:09 +0530

    fix: add missing semicolon and improve route organization in user controller and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser , refreshAccessToken} from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, getCurrentUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { changeCurrentPassword } from "../controllers/user.controller.js";
 
 let router = Router();
 
@@ -18,10 +19,49 @@ router.route("/login").post(
 )
 
 router.route("/logout").post(
-  verifyJWT, logoutUser
+  verifyJWT, 
+  logoutUser
 )
 
 router.route("/refresh-token").post(
   refreshAccessToken
 )
+
+router.route("/change-password").post(
+  verifyJWT, 
+  changeCurrentPassword
+);
+
+router.route("/current-user").get(
+  verifyJWT, 
+  getCurrentUser
+);
+
+router.route("/update-account").patch(
+  verifyJWT, 
+  updateAccountDetails
+);
+
+router.route("/avatar").patch(
+  verifyJWT, 
+  upload.single("avatar"), 
+  updateUserAvatar);
+
+router.route("/cover-image").patch(
+  verifyJWT, 
+  upload.single("coverImage"), 
+  updateUserCoverImage
+);
+
+router.route("/c/:username").get(
+  verifyJWT, 
+  getUserChannelProfile
+);
+
+router.route("/history").get(
+  verifyJWT, 
+  getWatchHistory
+);
+
+
 export default router;
